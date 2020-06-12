faba8845f35bdd26fa7bf2c0f0fa9bef25925661
budi-injo <budi.injo@telepathy.ai>
2020-06-12 12:48:20 +0800

    update mockup data

--- src/Screens/InvoiceListScreen/index.js
+++ src/Screens/InvoiceListScreen/index.js
@@ -1,53 +1,110 @@
 import React from 'react';
 import { SafeAreaView, View, FlatList, StyleSheet, Text , TouchableOpacity} from 'react-native';
 import AppButton from '../../Components/AppButton';
+import InvoiceItem from './InvoiceItem';
 
 const DATA = [
 	{
 		id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
 		title: 'First Item',
+		invoiceId: "643",
+		merchantId: "M1",
+		transactionDate: "2020-05-29",
+		dueDate: "2020-05-30",
+		totalTax: 0.00,
+		totalAmount: 320.00,
+		balanceAmount: 320.00
 	},
 	{
 		id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
 		title: 'Second Item',
+		invoiceId: "644",
+		merchantId: "M2",
+		transactionDate: "2020-05-29",
+		dueDate: "2020-05-30",
+		totalTax: 0.00,
+		totalAmount: 320.00,
+		balanceAmount: 320.00
 	},
 	{
 		id: '58694a0f-3da1-471f-bd96-145571e29d721',
 		title: 'Third Item',
+		invoiceId: "645",
+		merchantId: "M2",
+		transactionDate: "2020-05-29",
+		dueDate: "2020-05-30",
+		totalTax: 0.00,
+		totalAmount: 320.00,
+		balanceAmount: 320.00
 	},
 	{
 		id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba2',
 		title: 'First Item',
+		invoiceId: "646",
+		merchantId: "M2",
+		transactionDate: "2020-05-29",
+		dueDate: "2020-05-30",
+		totalTax: 0.00,
+		totalAmount: 320.00,
+		balanceAmount: 320.00
 	},
 	{
 		id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f633',
 		title: 'Second Item',
+		invoiceId: "647",
+		merchantId: "M2",
+		transactionDate: "2020-05-29",
+		dueDate: "2020-05-30",
+		totalTax: 0.00,
+		totalAmount: 320.00,
+		balanceAmount: 320.00
 	},
 	{
 		id: '58694a0f-3da1-471f-bd96-145571e29d724',
 		title: 'Third Item',
+		invoiceId: "648",
+		merchantId: "M2",
+		transactionDate: "2020-05-29",
+		dueDate: "2020-05-30",
+		totalTax: 0.00,
+		totalAmount: 320.00,
+		balanceAmount: 320.00
 	},
 	{
 		id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba5',
 		title: 'First Item',
+		invoiceId: "649",
+		merchantId: "M2",
+		transactionDate: "2020-05-29",
+		dueDate: "2020-05-30",
+		totalTax: 0.00,
+		totalAmount: 320.00,
+		balanceAmount: 320.00
 	},
 	{
 		id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f636',
 		title: 'Second Item',
+		invoiceId: "650",
+		merchantId: "M2",
+		transactionDate: "2020-05-29",
+		dueDate: "2020-05-30",
+		totalTax: 0.00,
+		totalAmount: 320.00,
+		balanceAmount: 320.00
 	},
 	{
 		id: '58694a0f-3da1-471f-bd96-145571e29d727',
 		title: 'Last Item',
+		invoiceId: "651",
+		merchantId: "M2",
+		transactionDate: "2020-05-29",
+		dueDate: "2020-05-30",
+		totalTax: 0.00,
+		totalAmount: 320.00,
+		balanceAmount: 320.00
 	},
 ];
 
-const Item = ({ title }) => {
-	return (
-		<View style={styles.item}>
-			<Text style={styles.title}>{title}</Text>
-		</View>
-	);
-}
 
 const InvoiceList = ({navigation}) => {
 
@@ -77,7 +134,7 @@ const InvoiceList = ({navigation}) => {
 
 			<FlatList
 				data={DATA}
-				renderItem={({ item }) => <Item title={item.title} />}
+				renderItem={({ item }) => <InvoiceItem data={item}/>}
 				keyExtractor={item => item.id}
 			/>
 
@@ -95,15 +152,6 @@ const styles = StyleSheet.create({
 	container: {
 		flex: 1,
 	},
-	item: {
-		backgroundColor: '#f9c2ff',
-		padding: 20,
-		marginVertical: 8,
-		marginHorizontal: 16,
-	},
-	title: {
-		fontSize: 32,
-	},
 	buttonTitle:{
 		color: 'white'
 	},
@@ -113,8 +161,8 @@ const styles = StyleSheet.create({
 		color:'white'
 	},
 	bottomPanel:{
-		backgroundColor: 'yellow',
 		justifyContent: 'space-around',
+		marginVertical: 10,
 		flexDirection:'row'
 	}
 });
