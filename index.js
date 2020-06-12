381edaacbe1032195767f1351bd9138bee1e55c2
budi-injo <budi.injo@telepathy.ai>
2020-06-12 11:48:28 +0800

    implement stack navigator

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 
 import {AppRegistry} from 'react-native';
 import InvoiceList from './src/Screens/InvoiceList';
+import CreateInvoice from './src/Screens/CreateInvoice';
+import App from './App'
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => InvoiceList);
+AppRegistry.registerComponent(appName, () => App);
