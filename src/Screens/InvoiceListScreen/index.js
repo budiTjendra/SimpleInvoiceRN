c60f08bc4fb3abfe9361e1f143b18683c0519994
budi-injo <budi.injo@telepathy.ai>
2020-06-14 13:53:19 +0800

    code refactoring

--- src/Screens/InvoiceListScreen/index.js
+++ src/Screens/InvoiceListScreen/index.js
@@ -3,7 +3,7 @@ import { SafeAreaView, View, FlatList, StyleSheet, Text, Button , TouchableWitho
 import InvoiceItem from './InvoiceItem';
 import Header from './Header';
 import InvoiceListContext from './InvoiceListContext';
-import {sortType, filterType} from './Constants'
+import {sortType, actionType} from './Constants'
 import Footer from './Footer';
 import produce from "immer"
 import _ from "underscore"
@@ -12,116 +12,7 @@ import { useSelector, useDispatch as useDispatchRedux } from 'react-redux'
 import * as actions from './../../Redux/Actions'
 import * as TimeFrameHelper from './TimeFrameHelper'
 
-
-const actionType = {
-	...sortType,
-	updateData: "update_data"
-}
-
-
-const DATA = [
-	{
-		id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-		title: 'First Item',
-		invoiceId: "700",
-		merchantId: "M1",
-		transactionDate: "2020-05-01",
-		dueDate: "2020-05-30",
-		totalTax: 0.00,
-		totalAmount: 100.00,
-		balanceAmount: 320.00
-	},
-	{
-		id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-		title: 'Second Item',
-		invoiceId: "644",
-		merchantId: "M2",
-		transactionDate: "2020-05-08",
-		dueDate: "2020-05-30",
-		totalTax: 0.00,
-		totalAmount: 500.00,
-		balanceAmount: 320.00
-	},
-	{
-		id: '58694a0f-3da1-471f-bd96-145571e29d721',
-		title: 'Third Item',
-		invoiceId: "645",
-		merchantId: "M3",
-		transactionDate: "2020-05-02",
-		dueDate: "2020-05-30",
-		totalTax: 0.00,
-		totalAmount: 1000.00,
-		balanceAmount: 320.00
-	},
-	{
-		id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba2',
-		title: 'First Item',
-		invoiceId: "646",
-		merchantId: "M4",
-		transactionDate: "2020-05-29",
-		dueDate: "2020-05-30",
-		totalTax: 0.00,
-		totalAmount: 320.00,
-		balanceAmount: 320.00
-	},
-	{
-		id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f633',
-		title: 'Second Item',
-		invoiceId: "647",
-		merchantId: "M5",
-		transactionDate: "2020-05-03",
-		dueDate: "2020-05-30",
-		totalTax: 0.00,
-		totalAmount: 60.00,
-		balanceAmount: 320.00
-	},
-	{
-		id: '58694a0f-3da1-471f-bd96-145571e29d724',
-		title: 'Third Item',
-		invoiceId: "648",
-		merchantId: "M6",
-		transactionDate: "2020-05-05",
-		dueDate: "2020-05-30",
-		totalTax: 0.00,
-		totalAmount: 70.00,
-		balanceAmount: 320.00
-	},
-	{
-		id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba5',
-		title: 'First Item',
-		invoiceId: "649",
-		merchantId: "M7",
-		transactionDate: "2020-05-04",
-		dueDate: "2020-05-30",
-		totalTax: 0.00,
-		totalAmount: 5.00,
-		balanceAmount: 320.00
-	},
-	{
-		id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f636',
-		title: 'Second Item',
-		invoiceId: "650",
-		merchantId: "M8",
-		transactionDate: "2020-05-06",
-		dueDate: "2020-05-30",
-		totalTax: 0.00,
-		totalAmount: 600.00,
-		balanceAmount: 320.00
-	},
-	{
-		id: '58694a0f-3da1-471f-bd96-145571e29d727',
-		title: 'Last Item',
-		invoiceId: "651",
-		merchantId: "M9",
-		transactionDate: "2020-05-07",
-		dueDate: "2020-05-30",
-		totalTax: 0.00,
-		totalAmount: 320.00,
-		balanceAmount: 320.00
-	},
-];
-
-const initialState = {data: DATA}
+const initialState = {data: []}
 
 function reducerFunction(draft, action) {
 	// reducer code goes here.
@@ -168,28 +59,27 @@ const InvoiceList = ({navigation}) => {
 	const dispatchRedux = useDispatchRedux();
 
 	React.useEffect(()=>{
-		const fetchInvoices = (token) => {
+
+		const fetchInvoices = async (token) => {
 			const entireHistory = TimeFrameHelper.EntireHistory();
-			const resp = services.getInvoices({
+			const resp = await services.getInvoices({
 				token,
 				fromDate: entireHistory.startDate,
 				toDate: entireHistory.endDate,
 				merchantReference: 7066823
 			});
-			return resp;
+			return resp
 		}
 
 		//get access token, then fetch list
-		new Promise((resolve, reject) =>{
-			const resp = services.getAccessToken()
-			resolve(resp)
-		})
+		services.getAccessToken()
 			.catch(resp => console.error('error in get AccessToken', {resp}))
-			.then(resp => resp.data)
+			.then(resp => {
+				return  resp.data
+			})
 			.then(data => {
 				dispatchRedux(actions.setToken(data))
-				const resp = fetchInvoices(data.access_token)
-				return resp;
+				return fetchInvoices(data.access_token);
 		})
 			.catch(resp => console.error('error in fetchInvoices', {resp}))
 			.then (resp => resp.data)
@@ -200,7 +90,7 @@ const InvoiceList = ({navigation}) => {
 					payload: data
 				})
 			})
-
+		
 	},[])
 
 	const {
